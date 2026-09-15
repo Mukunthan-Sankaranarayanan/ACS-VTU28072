91d0ef0bd4582313696f5065d1c2e518a8216151
Mukunthan-Sankaranarayanan <vtu28072@veltech.edu.in>
2026-09-15 14:10:59 +0530

    Create W6-Task 50.java

---
+++ W6-Task 50.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            k--;
+
+            if (k == 0) {
+                return current.val;
+            }
+
+            current = current.right;
+        }
+
+        return -1;
+    }
+}
